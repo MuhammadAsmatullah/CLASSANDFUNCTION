e56d5b10acca1c647bea216b388b2a3a21477527
Muhammad Asmatullah <masmatullah450@gmail.com>
2025-03-25 11:36:00 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Students
+{
+    public string Name;
+    public int RollNumber;
+    public char Grade;
+
+    public Students (string name , int rollNumber , char grade)
+    {
+        Name = name;
+        RollNumber = rollNumber;
+        Grade = grade;
+    } 
+    public void DisplayStudentDetails ()
+    {
+        Console.WriteLine($"Student Details: Name: {Name} Roll_NO: {RollNumber} Grade: {Grade}");
+    }
+
+    class Progarm
+    {
+        static void Main (string[] args)
+        {
+            Students s_details = new Students("Abdur Rehman", 2040, 'B');
+            s_details.DisplayStudentDetails();
+        }
+    }
+}
